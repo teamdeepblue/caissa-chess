385046badfb7fd40b08c87235c627f217737fe6d
Willem Dehaes <willemdehaes@gmail.com>
2016-08-31 21:21:18 +0000

    added devise gem and did basic configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #Configure action mailer for devise user authentication
+  config.action_mailer.default_url_options = { host: 'caissachess.herokuapp.com' }
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
