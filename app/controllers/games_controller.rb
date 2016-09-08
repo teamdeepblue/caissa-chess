c09bf2e8048aa35e08c771f2a06de353a339a5bd
Erin Morrissey <erinmorrissey@gmail.com>
2016-09-08 18:59:43 +0000

    Build #new & #create methods for 'games' controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,16 @@
 class GamesController < ApplicationController
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create(game_params)
+    redirect_to game_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name)
+  end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+  describe 'games#new action' do
+    it 'should successfully show the New Game form page' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 
+  describe 'games#create action' do
+    it 'should successfully create a new game in the database' do
+      post :create, game: {name: 'test game name'}
+      expect(response).to redirect_to game_path
+      game = Game.last
+      expect(game.name).to eq('test game name')
+    end
+  end
 end
