19b894a32e960d96e6ee7d8fe70282f4d05b01e3
Willem Dehaes <willemdehaes@gmail.com>
2016-09-10 13:10:11 +0000

    add is_obstructed method to piece model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,18 @@
 class Piece < ActiveRecord::Base
   belongs_to :game
   belongs_to :player
+
+  def is_obstructed?(x_destination, y_destination)
+    x_start = self.x_position
+    y_start = self.y_position
+    if y_start == y_destination #horizontal case
+      pieces = Piece.where(y_position: y_start, x_position: x_start..x_destination)
+    elsif x_start == x_destination #vertical case
+      # ...
+    elsif x_start - x_destination == y_start - y_destination #slope move
+      # ...
+    else #invalid move
+      # ...
+    end
+  end
 end
