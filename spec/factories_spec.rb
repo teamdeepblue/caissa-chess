5143f6ea6903a408f2b4fbf5970c8b84b3b5b0b7
Ernie Jamison <jamisonernie@gmail.com>
2016-09-10 13:55:34 +0000

    Fixed rubocop violations

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 FactoryGirl.factories.map(&:name).each do |factory_name|
   describe "The #{factory_name} factory" do
-     it 'is valid' do
+    it 'is valid' do
       expect(build(factory_name)).to be_valid
-     end
+    end
   end
 end
