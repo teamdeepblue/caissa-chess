d083beb03d6cdea032b0a60eb3523e3079c1d39b
Willem Dehaes <willemdehaes@gmail.com>
2016-09-10 14:13:45 +0000

    edited is_obstructed method

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -5,14 +5,23 @@ class Piece < ActiveRecord::Base
   def is_obstructed?(x_destination, y_destination)
     x_start = self.x_position
     y_start = self.y_position
+    pieces = []
     if y_start == y_destination #horizontal case
-      pieces = Piece.where(y_position: y_start, x_position: x_start..x_destination)
+      x_start < x_destination ? (x_start = x_start + 1) : (x_start = x_start - 1)
+      pieces = Piece.where(y_position: y_start, x_position: y_start..x_destination)
     elsif x_start == x_destination #vertical case
-      # ...
+      y_start < y_destination ? (y_start = y_start + 1) : (y_start = y_start - 1)
+      pieces = Piece.where(x_position: x_start, y_position: y_start..y_destination)      
     elsif x_start - x_destination == y_start - y_destination #slope move
-      # ...
+      x_start < x_destination ? (x_start = x_start + 1) : (x_start = x_start - 1)
+      (x_start..x_destination).each do |x|
+        y = x - x_start + y_start
+        pieces.concat(Piece.where(x_position: x, y_position: y))
+      end
     else #invalid move
-      # ...
+      raise 'This is not a valid move'
     end
+
+    return pieces.length > 0
   end
 end
