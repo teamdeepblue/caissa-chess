9899e85a92b8796a70476a301d020e7bde9e4cdc
Iliana Paleva <ip97@cornell.edu>
2016-09-14 04:03:08 +0000

    Created the update action in the games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,18 @@
 class GamesController < ApplicationController 
 	def show 
 		render template: "games/#{params[:game]}"
-	end
+	end 
+
+	def new 
+		@game = Game.new 
+	end 
+
+	def create 
+		@game = Game.create 
+	end 
+
+	def update 
+		@game = Game.find(params[:id]) 
+		current_player.update_attribute(:game_id, @game.id) 
+	end 
 end
