94e25b33583e47c23daa592bc6ed0ab910f19696
Ernie Jamison <jamisonernie@gmail.com>
2016-09-15 19:34:33 +0000

    Refactored

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,26 +3,39 @@ class Piece < ActiveRecord::Base
   belongs_to :player
 
   def is_obstructed?(x_destination, y_destination)
-    x_start = self.x_position
-    y_start = self.y_position
-    pieces = []
-    if y_start == y_destination #horizontal case
-      x_start < x_destination ? (x_start = x_start + 1) : (x_start = x_start - 1)
-      pieces = Piece.where(game_id: game_id, y_position: y_start, x_position: y_start..x_destination)
-    elsif x_start == x_destination #vertical case
-      y_start < y_destination ? (y_start = y_start + 1) : (y_start = y_start - 1)
-      pieces = Piece.where(game_id: game_id, x_position: x_start, y_position: y_start..y_destination)      
-    elsif x_start - x_destination == y_start - y_destination #slope move
-      x_start < x_destination ? (x_start = x_start + 1) : (x_start = x_start - 1)
-      (x_start..x_destination).each do |x|
-        y = x - x_start + y_start
-        pieces.concat(Piece.where(game_id: game_id, x_position: x, y_position: y))
-      end
-    else #invalid move
-      raise 'This is not a valid move'
-    end
+    x_start = x_position
+    y_start = y_position
+    @x_destination = x_destination
+    @y_destination = y_destination
 
-    return pieces.length > 0
+    if moving_horizontally?
+      x_start = x_position + 1
+      x_end = x_destination - 1
+      return Piece.where(game_id: game.id, y_position: y_start, x_position: (x_start..x_destination)).exists?
+    elsif moving_vertically?
+      y_start = y_position + 1
+      y_end = y_destination - 1
+      return Piece.where(game_id: game.id, x_position: x_start, y_position: (y_start..y_end)).exists?
+    elsif moving_diagonally?    # needs word
+      x_start = x_position + 1
+      x_end = x_destination - 1
+      (x_start..x_destination).each do |x|
+        y = x_destination - x_start + y_start
+      end
+      return Piece.where(game_id: game.id, y_position: y_start, x_position: (x_start..x_destination)).exists?
+    end
+  end
+
+  def moving_horizontally?
+    y_position == @y_destination
+  end
+
+  def moving_vertically?
+    x_position == @x_destination
+  end
+
+  def moving_diagonally?
+    (x_position - @x_destination == y_position - @y_destination)
   end
 end
 
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  describe "is_obstructed?" do
-    it "should detect an obstructing piece horizontally" do
-      game = Game.create()
+  describe 'is_obstructed?' do
+    it 'should detect an obstructing piece horizontally' do
+      game = Game.create
       piece = Piece.create(game_id: game.id, x_position: 0, y_position: 0)
       Piece.create(game_id: game.id, x_position: 3, y_position: 0)
-      Piece.create(game_id: game.id, x_position: 6, y_position: 0)
+      Piece.create(game_id: game.id, x_position: 5, y_position: 0)
       expect(piece.is_obstructed?(7, 0)).to be true
     end
 
     it "should detect an obstructing piece vertically" do
-      game = Game.create()
+      game = Game.create
       piece = Piece.create(game_id: game.id, x_position: 0, y_position: 0)
+      # byebug
       Piece.create(game_id: game.id, x_position: 0, y_position: 2)
       Piece.create(game_id: game.id, x_position: 0, y_position: 6)
       expect(piece.is_obstructed?(0, 7)).to be true
     end
 
     it "should detect an obstructing piece diagonally" do
-      game = Game.create()
+      game = Game.create
       piece = Piece.create(game_id: game.id, x_position: 0, y_position: 0)
       Piece.create(game_id: game.id, x_position: 0, y_position: 0)
       Piece.create(game_id: game.id, x_position: 4, y_position: 4)
