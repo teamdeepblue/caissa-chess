391f8a6d7cfea882266aeb537f3e9615e2ec6cc4
Erin Morrissey <erinmorrissey@gmail.com>
2016-09-16 00:09:31 +0000

    Create player when new game is created

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,13 @@
 class GamesController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @game = Game.new
   end
 
   def create
     @game = Game.create(game_params)
+    @game.players.create(game_id: @game.id, user_id: current_user.id, color: [:black, :white].sample)
     redirect_to game_path(@game)
   end
 
