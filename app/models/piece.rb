27bb4d5450f61e25bfc3b24b043ce1457a21aa50
Ernie Jamison <jamisonernie@gmail.com>
2016-09-17 18:10:21 +0000

    Changed is_obstructed to return an array instead of boolean

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,28 +2,31 @@ class Piece < ActiveRecord::Base
   belongs_to :game
   belongs_to :player
 
-  def is_obstructed?(x_destination, y_destination)
+  def obstructing_pieces(x_destination, y_destination)
     x_start = x_position
     y_start = y_position
     @x_destination = x_destination
     @y_destination = y_destination
+    pieces = []
 
     if moving_horizontally?
       x_start = x_position + 1
       x_end = x_destination - 1
-      return Piece.where(game_id: game.id, y_position: y_start, x_position: (x_start..x_destination)).exists?
+      pieces = Piece.where(game_id: game.id, y_position: y_start, x_position: (x_start + 1..x_destination - 1)).exists?
     elsif moving_vertically?
       y_start = y_position + 1
       y_end = y_destination - 1
-      return Piece.where(game_id: game.id, x_position: x_start, y_position: (y_start..y_end)).exists?
+      pieces = Piece.where(game_id: game.id, x_position: x_start, y_position: (y_start + 1..y_destination - 1)).exists?
     elsif moving_diagonally?    # needs word
-      x_start = x_position + 1
-      x_end = x_destination - 1
-      (x_start..x_destination).each do |x|
-        y = x_destination - x_start + y_start
+      (x_start + 1..x_destination - 1).each do |x|
+        y = x - x_start + y_start
+        pieces.concat(Piece.where(x_position: x, y_position: y))
       end
-      return Piece.where(game_id: game.id, y_position: y_start, x_position: (x_start..x_destination)).exists?
     end
+  end
+
+  def is_obstructed?(x_destination, y_destination)
+    obstructing_pieces(x_destination, y_destination).exists?
   end
 
   def moving_horizontally?
