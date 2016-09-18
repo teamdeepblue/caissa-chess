6db5e5451e5c2b0a22b8c2c9dc4b40b5c6f9628a
Iliana Paleva <ip97@cornell.edu>
2016-09-18 14:04:32 +0000

    Controller correction

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,14 +3,6 @@ class GamesController < ApplicationController
 		render template: "games/#{params[:game]}"
 	end 
 
-	def new 
-		@game = Game.new 
-	end 
-
-	def create 
-		@game = Game.create 
-	end 
-
 	def update 
 		@game = Game.find(params[:id]) 
 		current_player.update_attribute(:game_id, @game.id) 
