2b77fd0412bd88371ae9877597402edc5648dce9
Shreyams Jain <shreyams.jain@sjsu.edu>
2016-09-25 07:24:34 +0000

    Fix rubocop errors

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,23 +1,33 @@
 class PiecesController < ApplicationController
-	def new
-		white_pieces = ["rook_white", "knight_white", "bishop_white", "king_white", "queen_white", "bishop_white", "knight_white", "rook_white"]
-  		black_pieces = ["rook_black", "knight_black", "bishop_black", "queen_black", "king_black", "bishop_black", "knight_black", "rook_black"]
-  		white_x_pos = 1
-  		for wp in white_pieces
-  			Piece.create(piece_name: wp, x_position: white_x_pos, y_position: 1)
-  			white_x_pos += 1
-  		end
-  		black_x_pos = 1
-  		for bp in black_pieces
-  			Piece.create(piece_name: bp, x_position: black_x_pos, y_position: 8)
-  			black_x_pos += 1
-  		end
-  		pawn_pos = 1
-  		8.times do
-  			Piece.create(piece_name: "pawn_white", x_position: pawn_pos, y_position: 2)
-  			Piece.create(piece_name: "pawn_black", x_position: pawn_pos, y_position: 7)
-  			pawn_pos += 1
-  		end
-  		render json: Piece.all
-	end
+  def new
+    set_white_pieces
+    set_black_pieces
+    pawn_pos = 1
+    8.times do
+      Piece.create(piece_name: 'pawn_white', x_position: pawn_pos, y_position: 2)
+      Piece.create(piece_name: 'pawn_black', x_position: pawn_pos, y_position: 7)
+      pawn_pos += 1
+    end
+    render json: Piece.all
+  end
+
+  private
+
+  def set_white_pieces
+    white_pieces = %w(rook_white knight_white bishop_white king_white queen_white bishop_white knight_white rook_white)
+    white_x_pos = 1
+    white_pieces.each do |wp|
+      Piece.create(piece_name: wp, x_position: white_x_pos, y_position: 1)
+      white_x_pos += 1
+    end
+  end
+
+  def set_black_pieces
+    black_pieces = %w(rook_black knight_black bishop_black queen_black king_black bishop_black knight_black rook_black)
+    black_x_pos = 1
+    black_pieces.each do |bp|
+      Piece.create(piece_name: bp, x_position: black_x_pos, y_position: 8)
+      black_x_pos += 1
+    end
+  end
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,13 +2,12 @@ class Piece < ActiveRecord::Base
   belongs_to :game
   belongs_to :player
   before_create :validate_max_pieces
-  enum piece_name: [ :king_black, :king_white, :queen_black, :queen_white, 
-  	:rook_black, :rook_white, :bishop_black, :bishop_white, :knight_black, :knight_white, :pawn_black, :pawn_white ]
-  validates :x_position, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 8}, allow_nil: false
-  validates :y_position, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 8}
+  enum piece_name: [:king_black, :king_white, :queen_black, :queen_white, :rook_black, :rook_white, :bishop_black, :bishop_white, :knight_black, :knight_white, :pawn_black, :pawn_white]
+  validates :x_position, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 8 }, allow_nil: false
+  validates :y_position, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 8 }
 
-	def validate_max_pieces
-	  errors[:base] << "Only 32 pieces are allowed." if Piece.count == 32
-	  errors.empty?
-	end
+  def validate_max_pieces
+    errors[:base] << 'Only 32 pieces are allowed.' if Piece.count == 32
+    errors.empty?
+  end
 end
