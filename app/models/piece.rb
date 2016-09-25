a6bcbeb1f642d36493c003c7ba5e9d9305226350
Ernie Jamison <jamisonernie@gmail.com>
2016-09-25 18:31:16 -0400

    Reverted back to returning true/false

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,17 +2,16 @@ class Piece < ActiveRecord::Base
   belongs_to :game
   belongs_to :player
 
-  def get_obstructing_pieces(x_destination, y_destination)
+  def is_obstructed?(x_destination, y_destination)
     x_start = x_position
     y_start = y_position
     @x_destination = x_destination
     @y_destination = y_destination
-    pieces = []
 
     if moving_horizontally?
-      pieces = Piece.where(game_id: game.id, y_position: y_start, x_position: (x_start + 1..x_destination - 1)).exists?
+      return Piece.where(game_id: game.id, y_position: y_start, x_position: (x_start + 1..x_destination - 1)).exists?
     elsif moving_vertically?
-      pieces = Piece.where(game_id: game.id, x_position: x_start, y_position: (y_start + 1..y_destination - 1)).exists?
+      return Piece.where(game_id: game.id, x_position: x_start, y_position: (y_start + 1..y_destination - 1)).exists?
     elsif moving_diagonally? # needs word
       (x_start + 1..x_destination - 1).each do |x|
         y = x - x_start + y_start
