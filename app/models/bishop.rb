03916e19de1b095d228e9b7b201df87ca98d807d
Erin Morrissey <erinmorrissey@gmail.com>
2016-09-28 00:10:22 +0000

    Add valid_move? for Bishop

---
+++ app/models/bishop.rb
@@ -0,0 +1,10 @@
+class Bishop < Piece
+  def valid_move?(x_destination, y_destination)
+    return false unless super(x_destination, y_destination)
+    return false unless obstructed?(x_destination, y_destination)
+    x_diff_val = diff(x_destination, x_position)
+    y_diff_val = diff(y_destination, y_position)
+    return true if x_diff_val == y_diff_val
+    false
+  end
+end
