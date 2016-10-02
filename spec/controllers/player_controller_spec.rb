065a376e511e25e3d8cd3bb5689d197e5b36f4b2
Iliana Paleva <ip97@cornell.edu>
2016-10-02 06:01:08 +0000

    Added a player and join game options/started out TDD tests

--- spec/controllers/player_controller_spec.rb
+++ spec/controllers/player_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe PlayerController, type: :controller do
+	describe "player#create action" do 
+		it "should successfully create a new player" do
+			g = FactoryGirl.create(:game)
+			u = FactoryGirl.create(:user)
+			sign_in u
 
+			post :create, game_id: g.id
+			#test for redirection to correct path
+			expect(g.players.length).to 
+		end 
 end
