c12a833167702a4ce0d856f85a1d72a2c8521e7e
Willem Dehaes <willemdehaes@gmail.com>
2016-10-05 20:09:34 +0000

    relationship status with rubocop: it's complicated

--- app/models/pieces/rook.rb
+++ app/models/pieces/rook.rb
@@ -5,6 +5,6 @@ class Rook < Piece
     x_diff_val = diff(x_destination, x_position)
     y_diff_val = diff(y_destination, y_position)
     # return false if Rook tries to move in more than one direction at the time
-    return !(x_diff_val > 0 && y_diff_val > 0)
+    !(x_diff_val > 0 && y_diff_val > 0)
   end
 end
