dac2dbf6e32dcde95fd2fe27b20759c4d6b9fb6c
Willem Dehaes <willemdehaes@gmail.com>
2016-10-10 21:48:37 +0000

    changed colors to symbols

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -10,7 +10,7 @@ class PiecesController < ApplicationController
 
   def set_white_pieces
     white_pieces = %w(Rook Knight Bishop King Queen Bishop Knight Rook)
-    player = Player.where(['color = ? and game_id = ?', 'White', @game.id])
+    player = Player.where(['color = ? and game_id = ?', :white, @game.id])
     white_x_pos = 0
     white_pieces.each do |wp|
       Piece.create(game_id: @game.id, player_id: player.ids[0], type: wp, x_position: white_x_pos, y_position: 0)
@@ -21,7 +21,7 @@ class PiecesController < ApplicationController
 
   def set_black_pieces
     black_pieces = %w(Rook Knight Bishop Queen King Bishop Knight Rook)
-    player = Player.where(['color = ? and game_id = ?', 'Black', @game.id])
+    player = Player.where(['color = ? and game_id = ?', :black, @game.id])
     black_x_pos = 0
     black_pieces.each do |bp|
       Piece.create(game_id: @game.id, player_id: player.ids[0], type: bp, x_position: black_x_pos, y_position: 7)
