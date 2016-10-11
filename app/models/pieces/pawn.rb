a4c4e129f80e704b74a0c3f7662d1527c791809f
Willem Dehaes <willemdehaes@gmail.com>
2016-10-11 18:41:40 +0000

    refactoring + rubocop fixes

--- app/models/pieces/pawn.rb
+++ app/models/pieces/pawn.rb
@@ -1,15 +1,17 @@
 class Pawn < Piece
-  # rubocop:disable all
+  # rubocop: disable AbcSize, CyclomaticComplexity, MethodLength, PerceivedComplexity
   def valid_move?(x_destination, y_destination)
     # check for Piece valid_move? conditions
     return false unless super(x_destination, y_destination)
+    diff_x = diff(x_destination, x_position)
+    diff_y = diff(y_destination, y_position)
     # a pawn can never move horizontally or, and never move more than two fields ahead, or more than one sideways
-    return false if moving_horizontally?(y_destination) || diff(x_destination, x_position) > 1 || diff(y_destination, y_position) > 2
-    #a pawn cannot move backwards
-    return false if (player.color == 'white' ? y_destination <= y_position : y_destination >= y_position)
+    return false if moving_horizontally?(y_destination) || diff_x > 1 || diff_y > 2
+    # a pawn cannot move backwards
+    return false if player.color == 'white' ? y_destination <= y_position : y_destination >= y_position
     # a pawn can only move 2 squares forward from its starting position
-    return false if diff(y_destination, y_position) == 2 && y_position != (player.color == 'white' ? 1 : 6)
-    if diff(x_destination, x_position).zero?
+    return false if diff_y == 2 && y_position != (player.color == 'white' ? 1 : 6)
+    if diff_x.zero?
       # if the pawn moves straight forward, the destination field cannot contain a piece
       !Piece.where(game_id: game.id, y_position: y_destination, x_position: x_destination).exists?
     else
@@ -18,5 +20,5 @@ class Pawn < Piece
       # to do: take into account en passant
     end
   end
-  # rubocop:enable all
+  # rubocop: enable all
 end
